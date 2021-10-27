3ef540e577c26ea77b01278235a728d62f89ffac
shiva695 <shiva6sj11@gmail.com>
2021-10-27 16:49:11 +0530

    papa react config

---
+++ Components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Image from "next/image";
+const Header = () => {
+  return (
+    <header className="">
+      <h1>this is the hrader</h1>
+      <Image
+        className="object-contain"
+        src="https://links.papareact.com/ua6"
+        width={200}
+        height={100}
+      />
+    </header>
+  );
+};
+
+export default Header;
