3d93643d2ef2568732d867998a8af3ea2d62c553
Neha Nikhat <nehanikhat18@gmail.com>
2021-10-28 15:39:12 +0530

    created navbar and added scrollbar-hide package

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "../components/header";
+import Nav from "../components/nav";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       </Head>
 
       <Header />
+      <Nav />
     </div>
   );
 }
