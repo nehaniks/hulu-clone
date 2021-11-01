3c62c2bd34441e84008dd1f877e501f447114c29
Neha Nikhat <nehanikhat18@gmail.com>
2021-11-01 11:05:41 +0530

    thumbnail grid view for different screens

--- components/thumbnail.js
+++ components/thumbnail.js
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import { ThumbUpIcon } from "@heroicons/react/outline";
+import { forwardRef } from "react";
 
-export default function Thumbnail({ result }) {
+const Thumbnail = forwardRef(({ result }, ref) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
   return (
-    <div className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+    <div
+      ref={ref}
+      className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
+    >
       <Image
         layout="responsive"
         src={
@@ -33,4 +37,8 @@ export default function Thumbnail({ result }) {
       </div>
     </div>
   );
-}
+});
+
+Thumbnail.displayName = "Thumbnail";
+
+export default Thumbnail;
